7ae758041c0f934142580b3ec15a5696292c8d19
HKarpenko <45368181+HKarpenko@users.noreply.github.com>
2021-01-09 19:37:23 +0100

    Split data on train and test, calc accuracy: only 5% :(

--- DecisionMaker/tree.py
+++ DecisionMaker/tree.py
@@ -1,25 +1,27 @@
 from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 target = 'popularity'
-feature = ['valence','year','acousticness','danceability','duration_ms','energy','explicit','instrumentalness','key','liveness','loudness','mode','speechiness','tempo']
+feature = ['valence','year','acousticness','danceability','duration_ms','energy','explicit','instrumentalness','liveness','loudness','mode','speechiness','tempo']
 data_path = "../data/data_by_decade/data_from_"
 
 def create_tree(decade = "00s"):
-    myTree = tree.DecisionTreeClassifier(criterion="gini", splitter="best")
-    print("1")
-    print(data_path + decade + ".csv")
+    myTree = tree.DecisionTreeClassifier(criterion="entropy", max_depth=6) # the best crit & depth
+
     df = pd.read_csv(str(data_path + decade + ".csv"))
-    print("2")
-    print(df)
-    myTree.fit(df[feature], df[target])
-    sample = [[0.613,2000,0.14300000000000002,0.843,270507,0.8059999999999999,1,0.0,4,0.0771,-5.9460000000000015,0,0.269,94.948]]
-    print(sample)
-    ans = myTree.predict(sample)
-    print(ans)
+
+    X_train, X_test, y_train, y_test = train_test_split(df[feature], df[target], test_size=0.3, random_state=1)
+
+    myTree.fit(X_train, y_train)
+
+    ans = myTree.predict(X_test)
+
+    print("Accuracy:", metrics.accuracy_score(y_test, ans))
 
 
 create_tree()
