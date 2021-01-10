2ceedd057edc05691b8139c0493d3aa8a6b9f4ac
HKarpenko <45368181+HKarpenko@users.noreply.github.com>
2021-01-10 21:00:54 +0100

    Random forest version

--- DecisionMaker/tree.py
+++ DecisionMaker/tree.py
@@ -1,6 +1,6 @@
 from sklearn import tree
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,23 +8,47 @@ import pandas as pd
 
 target = 'popularity'
 feature = ['valence','year','acousticness','danceability','duration_ms','energy','explicit','instrumentalness','liveness','loudness','mode','speechiness','tempo']
-data_path = "../data/data_by_decade/data_from_"
+data_path = "../data/data_by_decade/data_from_10s.csv"
 
-def create_tree(decade = "00s"):
-    print("DECADE: "+decade+"\n")
+def check_answers(answer, actual):
+    answer = list(answer)
+    actual = list(actual)
+    ok = 0
+    diff = 5
+    for i in range(len(answer)):
+        if(actual[i] < answer[i]+diff and actual[i] > answer[i]-diff ):
+            ok+=1
+    return ok/len(answer)
+
+def create_tree():
     for dp in range(1,15):
-        myTree = tree.DecisionTreeClassifier(criterion="gini", max_depth=dp) # the best crit & depth
+        myTree = tree.DecisionTreeClassifier(criterion="entropy", max_depth=dp) # the best crit & depth
 
-        df = pd.read_csv(str(data_path + decade + ".csv"))
+        df = pd.read_csv(str(data_path))
 
-        X_train, X_test, y_train, y_test = train_test_split(df[feature], df[target], test_size=0.3, random_state=1)
+        X_train, X_test, y_train, y_test = train_test_split(df[feature], df[target], test_size=0.2, random_state=1)
 
         myTree.fit(X_train, y_train)
 
         ans = myTree.predict(X_test)
+        print("Depth:",dp," Accuracy: ", check_answers(ans, y_test))
 
-        print("depth:",dp," Accuracy:", metrics.accuracy_score(y_test, ans))
+def create_forest():
+    dp=9
+    est=95
 
-create_tree("00s")
-for i in range(10,100,10):
-    create_tree(str(i)+"s")
+    df = pd.read_csv(str(data_path))
+
+    X_train, X_test, y_train, y_test = train_test_split(df[feature], df[target], test_size=0.2, random_state=1)
+
+    forest = RandomForestClassifier(n_estimators=est, max_depth=dp, random_state=0, criterion="gini") # the best num, crit & depth
+
+    forest.fit(X_train, y_train)
+    ans = forest.predict(X_test)
+
+    print("Num:", est, " Depth:", dp, " Accuracy: ", check_answers(ans, y_test))
+
+
+create_forest()
+
+
